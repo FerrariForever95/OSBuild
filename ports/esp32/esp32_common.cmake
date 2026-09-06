17fcdf45b8da7599d784f50957c693f0f65b3b0d
OSBuild User <you@example.com>
2026-09-06 11:39:38 +0530

    Build: ESP32_GENERIC_S3 — fix_20260906_113455 (2026-09-06 11:34)

--- ports/esp32/esp32_common.cmake
+++ ports/esp32/esp32_common.cmake
@@ -156,6 +156,8 @@ list(APPEND MICROPY_SOURCE_PORT
     modgyro.c
     modcar.c
     modsaturn.c
+    modsolarsys.c
+
 )
 list(TRANSFORM MICROPY_SOURCE_PORT PREPEND ${MICROPY_PORT_DIR}/)
 list(APPEND MICROPY_SOURCE_PORT ${CMAKE_BINARY_DIR}/pins.c)
